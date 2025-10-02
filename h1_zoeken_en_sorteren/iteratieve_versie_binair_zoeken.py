f2c96d12f79c6fd5673b031742bd86e5097ae201
Mauro M <81922575+MaSeraphina@users.noreply.github.com>
2025-10-02 09:40:27 +0200

    Update iteratieve_versie_binair_zoeken.py

--- h1_zoeken_en_sorteren/iteratieve_versie_binair_zoeken.py
+++ h1_zoeken_en_sorteren/iteratieve_versie_binair_zoeken.py
@@ -1,3 +1,26 @@
 
-def zoek_binair():
-  pass
+
+def zoek_binair(rij, getal):
+  links = 0
+  rechts = len(rij) - 1
+
+  while links != rechts: # lengte rij > 1
+    print(f'{links}, {rechts}')
+
+    midden = (links+rechts)//2
+    if rij[midden] < getal:
+      links = midden+1 # verder zoeken rechterkant
+    else:
+      rechts = midden
+
+  # 1 getal over
+  if rij[links] == getal:
+    #gevonden
+    return links
+  else: 
+    return -1
+
+
+# mijn_rij = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+# getal  = 70
+# print(zoekBinair(mijn_rij, getal))
