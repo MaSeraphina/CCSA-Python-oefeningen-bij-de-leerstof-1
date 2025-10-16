a4a020180a26d13b8ce31bc078dc2034389ecbec
Mauro M <81922575+MaSeraphina@users.noreply.github.com>
2025-10-16 08:58:00 +0200

    Update stapel_met_gelinkte_lijst.py

--- h2_gelinkte_lijsten/stapel_met_gelinkte_lijst.py
+++ h2_gelinkte_lijsten/stapel_met_gelinkte_lijst.py
@@ -2,24 +2,32 @@ class StackList:
 
     class Knoop:
         def __init__(self, data=None, volgende=None):
-            pass
+            self.data = data
+            self.volgende = volgende
 
     def __init__(self):
-        pass
+        self.top = StackList.Knoop()
 
     def is_empty(self):
-        pass
+        return self.top.data is None
 
     def push(self, data):
-        pass
+        hulp = StackList.Knoop() 
+        hulp.data = data
+        hulp.volgende = self.top
+        self.top = hulp
 
     def peek(self):
-        pass
+        return self.top.data
 
     def pop(self):
-        pass
-        
+        x = self.top.data
+        self.top = self.top.volgende
+        return x
 
 
-
-    
+s = StackList()
+print(s.is_empty())
+s.push(5)
+s.push(2)
+print(s.is_empty())
